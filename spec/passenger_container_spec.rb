597273a8f05204bae252051d7b4f7ffa29d9c6aa
Elena Garrone <elena.15@hotmail.it>
2014-12-21 09:54:34 +0000

    fixed indentation in passanger_container_spec.rb

--- spec/passenger_container_spec.rb
+++ spec/passenger_container_spec.rb
@@ -4,31 +4,31 @@ class ContainerHolder; include PassengerContainer; end
 
 describe PassengerContainer do
 
-	let(:holder)    { ContainerHolder.new }
-	let(:passenger) { Passenger.new }
+  let(:holder)    { ContainerHolder.new }
+  let(:passenger) { Passenger.new }
 
-	def passenger_touched_in
-		passenger.add_credit(5)
-		passenger.touch_in
-	end
+  def passenger_touched_in
+    passenger.add_credit(5)
+    passenger.touch_in
+  end
 
-	it "should let a passenger in only if the passenger touched-in" do
-		passenger_touched_in
-		expect{ holder.let_in(passenger) }.to change{ holder.passenger_count }.to 1
-	end
+  it "should let a passenger in only if the passenger touched-in" do
+    passenger_touched_in
+    expect{ holder.let_in(passenger) }.to change{ holder.passenger_count }.to 1
+  end
 
-	it "should not let a passenger in if the passenger has not touched-in" do
-		expect(holder.let_in(passenger)).to eq("Please, touch-in to enter the station.")
-	end
+  it "should not let a passenger in if the passenger has not touched-in" do
+    expect(holder.let_in(passenger)).to eq("Please, touch-in to enter the station.")
+  end
 
-	it "should not accept people when it's full" do
-		200.times { passenger_touched_in; holder.let_in(passenger) }
-		expect{ passenger.touch_in; holder.let_in(passenger) }.to raise_error("Sorry, you can't access, it's full.")
-	end
+  it "should not accept people when it's full" do
+    200.times { passenger_touched_in; holder.let_in(passenger) }
+    expect{ passenger.touch_in; holder.let_in(passenger) }.to raise_error("Sorry, you can't access, it's full.")
+  end
 
-	it "should know if its full" do
-		200.times { passenger_touched_in; holder.let_in(passenger) }
-		expect(holder).to be_full
-	end
+  it "should know if its full" do
+    200.times { passenger_touched_in; holder.let_in(passenger) }
+    expect(holder).to be_full
+  end
 
 end
