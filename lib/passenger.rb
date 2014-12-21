fa92afe541881a33e6a5570a55bfbd7989c774d9
Elena Garrone <elena.15@hotmail.it>
2014-12-21 09:46:18 +0000

    fixed indentation in passenger.rb

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,31 +1,31 @@
 class Passenger
 
-	attr_reader :oyster_credit
+  attr_reader :oyster_credit
 
-	def initialize
-		@touch_in = false
-		@oyster_credit = 0
-	end
+  def initialize
+    @touch_in = false
+    @oyster_credit = 0
+  end
 
-	def add_credit(pounds)
-		@oyster_credit += pounds
-	end
+  def add_credit(pounds)
+    @oyster_credit += pounds
+  end
 
-	def touched?
-		@touch_in
-	end
+  def touched?
+    @touch_in
+  end
 
-	def touch_in
-		if @oyster_credit >= 2
-			@oyster_credit -= 2
-			return @touch_in = true
-		else
-			return "You don't have enough credit. Please top-up your oyster card."
-		end
-	end
+  def touch_in
+    if @oyster_credit >= 2
+      @oyster_credit -= 2
+      return @touch_in = true
+    else
+      return "You don't have enough credit. Please top-up your oyster card."
+    end
+  end
 
-	def touch_out
-		@touch_in = false
-	end
+  def touch_out
+    @touch_in = false
+  end
 
 end
