49dc0bc5308ab03ea6d870c93a60cc4abd4c010c
Elena Garrone <elena.15@hotmail.it>
2014-12-21 09:48:48 +0000

    fixed indentation in passenger_container.rb

--- lib/passenger_container.rb
+++ lib/passenger_container.rb
@@ -1,32 +1,32 @@
 module PassengerContainer
 
-	DEFAULT_CAPACITY = 200
+  DEFAULT_CAPACITY = 200
 
-	attr_reader :passengers
+  attr_reader :passengers
 
-	def passengers
-		@passengers ||= []
-	end
-
-	def passenger_count
-		passengers.count
-	end
-
-  def let_in(passenger)
-		if passenger.touched?
-	  	raise "Sorry, you can't access, it's full."  if full?
-	    passengers << passenger
-		else
-			return "Please, touch-in to enter the station."
-		end
+  def passengers
+    @passengers ||= []
   end
 
-	def capacity
-		@capacity ||= DEFAULT_CAPACITY
-	end
+  def passenger_count
+    passengers.count
+  end
 
-	def full?
-		 passenger_count >= capacity
-	end
+  def let_in(passenger)
+    if passenger.touched?
+      raise "Sorry, you can't access, it's full."  if full?
+      passengers << passenger
+    else
+      return "Please, touch-in to enter the station."
+    end
+  end
+
+  def capacity
+    @capacity ||= DEFAULT_CAPACITY
+  end
+
+  def full?
+    passenger_count >= capacity
+  end
 
 end
