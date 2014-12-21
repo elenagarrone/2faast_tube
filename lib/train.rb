9854b0d46116718b8039dfb25655ded62427840e
Elena Garrone <elena.15@hotmail.it>
2014-12-21 09:52:15 +0000

    fixed indentation in train.rb

--- lib/train.rb
+++ lib/train.rb
@@ -1,24 +1,24 @@
 class Train
 
-	include PassengerContainer
+  include PassengerContainer
 
-	attr_reader :carriages
+  attr_reader :carriages
 
-	def initialize
-		@carriages = []
-	end
+  def initialize
+    @carriages = []
+  end
 
-	def hold(carriage)
-		raise "You can't add anymore carriages" if carriage_count >= 5
-		carriages << carriage
-	end
+  def hold(carriage)
+    raise "You can't add anymore carriages" if carriage_count >= 5
+    carriages << carriage
+  end
 
-	def unhold(carriage)
-		carriages.delete(carriage)
-	end
+  def unhold(carriage)
+    carriages.delete(carriage)
+  end
 
-	def carriage_count
-		carriages.count
-	end
+  def carriage_count
+    carriages.count
+  end
 
 end
